50f7afff451fbb473caeb64dbfb0f5037e550947
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-21 18:38:12 +0100

    fixed right margin

--- lua/flowstate/utils.lua
+++ lua/flowstate/utils.lua
@@ -1,9 +1,9 @@
 local M = {}
 local config = require("flowstate.config")
 
--- Funkcja obliczająca marginesy i centrowanie
+-- Oblicz i zastosuj centrowanie poziome
 M.enable_centering = function()
-	local win_width = vim.api.nvim_get_option("columns") -- Szerokość ekranu
+	local win_width = vim.api.nvim_get_option("columns") -- Szerokość okna
 	local text_width = config.settings.text_width
 
 	if text_width >= win_width then
@@ -11,33 +11,31 @@ M.enable_centering = function()
 		return
 	end
 
-	-- Oblicz marginesy po obu stronach
+	-- Oblicz marginesy
 	local side_padding = math.floor((win_width - text_width) / 2)
 
-	-- Ustawienia centrowania
+	-- Manipulacja 'win_options' dla efektu wyśrodkowania
 	vim.opt.wrap = false -- Wyłącz zawijanie linii
-	vim.opt.colorcolumn = tostring(text_width + 1) -- Linia pomocnicza do wyrównania
 	vim.opt.scrolloff = 0 -- Wyzeruj margines pionowy
 	vim.opt.sidescrolloff = 0 -- Wyzeruj przesuwanie w poziomie
+	vim.api.nvim_win_set_option(0, "winhighlight", "Normal:Normal") -- Bez specjalnego stylu
+	vim.cmd(string.format("setlocal leftmargin=%d rightmargin=%d", side_padding, side_padding)) -- Marginesy
 
-	-- Ustawienia bocznych marginesów
-	vim.api.nvim_win_set_option(0, "winhighlight", "Normal:Normal")
+	-- Ukryj zbędne elementy interfejsu
 	vim.api.nvim_win_set_option(0, "foldcolumn", "0") -- Wyłącz kolumnę składania
 	vim.api.nvim_win_set_option(0, "signcolumn", "no") -- Ukryj kolumnę znaków
-	vim.api.nvim_win_set_option(0, "statuscolumn", string.rep(" ", side_padding))
 end
 
--- Funkcja przywracająca domyślne ustawienia
+-- Przywróć domyślne ustawienia
 M.disable_centering = function()
 	vim.opt.wrap = true -- Przywróć zawijanie linii
-	vim.opt.colorcolumn = "" -- Usuń linię pomocniczą
-	vim.opt.sidescrolloff = 0 -- Przywróć domyślny margines
-	vim.opt.scrolloff = 8 -- Przywróć domyślne marginesy pionowe
+	vim.opt.scrolloff = 8 -- Przywróć domyślny margines pionowy
+	vim.opt.sidescrolloff = 8 -- Przywróć domyślny margines poziomy
+	vim.cmd("setlocal leftmargin=0 rightmargin=0") -- Usuń marginesy
 
-	-- Przywrócenie domyślnej kolumny
+	-- Przywróć domyślny interfejs
 	vim.api.nvim_win_set_option(0, "foldcolumn", "1")
 	vim.api.nvim_win_set_option(0, "signcolumn", "yes")
-	vim.api.nvim_win_set_option(0, "statuscolumn", "")
 end
 
 return M
