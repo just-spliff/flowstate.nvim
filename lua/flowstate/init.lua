5ea56dfc87bd0d58791e461ef29e43dda7b96efc
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-21 18:22:21 +0100

    add toggle command

--- lua/flowstate/init.lua
+++ lua/flowstate/init.lua
@@ -1,6 +1,30 @@
 local M = {}
 
-local config = require("flowstate.config")
 local utils = require("flowstate.utils")
+local config = require("flowstate.config")
+
+-- Funkcja do włączania/wyłączania trybu flowstate
+M.toggle = function()
+	if config.is_active then
+		utils.close_window()
+		utils.restore_ui()
+		config.is_active = false
+	else
+		utils.center_window()
+		utils.hide_ui()
+		config.is_active = true
+	end
+end
+
+-- Rejestracja komendy Neovim
+M.setup = function()
+	vim.api.nvim_create_user_command(
+		"FlowstateToggle", -- Nazwa komendy
+		function()
+			M.toggle()
+		end, -- Wywołanie funkcji toggle
+		{ desc = "Toggle Flowstate mode" } -- Opis komendy
+	)
+end
 
 return M
